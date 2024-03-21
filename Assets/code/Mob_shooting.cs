14c1326cfb0b952e7e8683053aaf0d67ec09648b
Stefano Todaro <s.todaro4@studenti.uniba.it>
2024-03-21 12:35:10 +0100

    Migliorata animazione sparo delle api e rotazione verso il giocatore. Create nuove cartelle per i vari slime

--- Assets/code/Mob_shooting.cs
+++ Assets/code/Mob_shooting.cs
@@ -7,39 +7,82 @@ public class Mob_shooting : MonoBehaviour
     public GameObject bullet;
     public Transform bulletPos;
     public GameObject player;
+    private Animator anim;
+    public float range=6.5f;
+    public float maxAngle = 0;
+    public float minAngle =- 120;
 
     private float timer;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
+        anim = gameObject.GetComponent<Animator>();
         
     }
 
     // Update is called once per frame
     void Update()
-    {
+    {   //altezza del giocatore rispetto al mob
+        float playerRelativeY = player.transform.position.y - transform.position.y;
+
+        //distanza reltiva dal mob
         float distance = Vector2.Distance(transform.position, player.transform.position);
        
 
-        if (distance < 5)
+        if (distance <range)
         {
-            timer += Time.deltaTime;
 
-            if (timer > 2)
+            if (playerRelativeY < 0)
             {
-                timer = 0;
-                shoot();
+                timer += Time.deltaTime;
+
+                if (timer > 2.5f)
+                {
+                    timer = 0;
+                    RotateTowardsTarget();
+                    anim.SetBool("shoot", true);
+                }
             }
 
+        }
+        else
+        {
+             transform.rotation = Quaternion.identity;
         }
          
        
         
     }
 
-    void shoot()
+    public void shoot()
     {
         Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        
     }
-}
+
+    public void reload()
+    {
+        // Instantiate(bullet, bulletPos.position, Quaternion.identity);
+        anim.SetBool("shoot", false);
+    }
+
+    private void RotateTowardsTarget()
+    {
+        
+        Vector2 directionToPlayer = player.transform.position - transform.position;
+        directionToPlayer.Normalize();
+
+        // Calcola l'angolo di rotazione in base alla direzione del giocatore
+        float angleToPlayer = Mathf.Atan2(directionToPlayer.y, directionToPlayer.x) * Mathf.Rad2Deg;
+        var offeset = 90f;
+
+
+        // Verifica se l'angolo verso il giocatore è all'interno del range
+        if (angleToPlayer > minAngle && angleToPlayer < maxAngle)
+        {
+            // L'angolo verso il giocatore è all'interno del range, quindi gira verso di lui
+            transform.rotation = Quaternion.Euler(Vector3.forward * (angleToPlayer + offeset));
+        }
+    }
+    }
