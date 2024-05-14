19b469aa10380eabe0c852ba74ee615778f058bf
Stefano Todaro <s.todaro4@studenti.uniba.it>
2024-05-14 10:42:13 +0200

    Migliorato il rapporto tra il rilascio di oggetti inglobati e il meccanismo di patrolling dei nemici

--- Assets/code/Mob_patrol.cs
+++ Assets/code/Mob_patrol.cs
@@ -7,7 +7,10 @@ public class Mob_patrol : MonoBehaviour
     public float moveSpeed = 3f; // Velocità di movimento del nemico
     public Transform[] patrolPoints; // Punti di pattuglia che il nemico deve seguire
     private int currentPatrolIndex = 0; // Indice del punto di pattuglia corrente
-    private bool movingForward = true; // Flag per tenere traccia della direzione di movimento
+    // Flag per tenere traccia della direzione di movimento
+    //da settare in base allo sprite iniziale
+    private bool movingRight = false; 
+    public bool isPatrolling=true;
     private Animator anim;
 
 
@@ -15,34 +18,41 @@ public class Mob_patrol : MonoBehaviour
     void Start()
     {
         anim = gameObject.GetComponent<Animator>();
+        currentPatrolIndex = 0;
        
     }
 
     // Update is called once per frame
     void Update()
     {
-        // Controlla se il nemico è arrivato al punto di pattuglia corrente
-        if (Vector2.Distance(transform.position, patrolPoints[currentPatrolIndex].position) < 0.1f)
+        if (isPatrolling)
         {
-            // Se il nemico è arrivato all'ultimo punto di pattuglia, inverti la direzione
-            if (currentPatrolIndex == patrolPoints.Length - 1)
+            // Controlla se il nemico è arrivato al punto di pattuglia corrente
+            if (Vector2.Distance(transform.position, patrolPoints[currentPatrolIndex].position) < 1f)
             {
-                movingForward = false;
-            }
-            // Se il nemico è arrivato al primo punto di pattuglia, torna a muoversi in avanti
-            else if (currentPatrolIndex == 0)
-            {
-                movingForward = true;
-            }
-            currentPatrolIndex += movingForward ? 1 : -1;
+                currentPatrolIndex+=1;
+                
+                if(currentPatrolIndex>= patrolPoints.Length)
+                {
+                    currentPatrolIndex = 0;
+                }
 
-            // Inverti la scala sull'asse X per girare lo sprite nella direzione corretta
-            anim.SetFloat("speed", moveSpeed);
-            gameObject.GetComponent<SpriteRenderer>().flipX = movingForward;
+                if (gameObject.transform.position.x<= patrolPoints[currentPatrolIndex].position.x)
+                {
+                    movingRight = true;
+                }
+                else
+                {
+                    movingRight = false;
+                }
+
+                // Inverti la scala sull'asse X per girare lo sprite nella direzione corretta
+                anim.SetFloat("speed", moveSpeed);
+                gameObject.GetComponent<SpriteRenderer>().flipX = movingRight;
+            }
+
+            // Muovi il nemico verso il punto di pattuglia corrente
+            transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPatrolIndex].position, moveSpeed * Time.deltaTime);
         }
-
-        // Muovi il nemico verso il punto di pattuglia corrente
-        transform.position = Vector2.MoveTowards(transform.position, patrolPoints[currentPatrolIndex].position, moveSpeed * Time.deltaTime);
-
     }
 }
--- Assets/code/Slime_bird_double_jump.cs
+++ Assets/code/Slime_bird_double_jump.cs
@@ -10,6 +10,8 @@ public class Slime_bird_double_jump : MonoBehaviour
     public Animator anim;
     public float DoubleJumpForce = 5.5f;
     public bool jumped = false;
+    //booleano che verifica se il personaggio sia su una cassa
+    public bool onBox=false;
 
 
     // Start is called before the first frame update
@@ -47,7 +49,7 @@ public class Slime_bird_double_jump : MonoBehaviour
             anim.SetBool("doubleJump", false);
         }
 
-        if (mov.isGrounded && jumped)
+        if (mov.isGrounded && jumped && !onBox)
         {
             jumped = false;
         }
@@ -59,5 +61,29 @@ public class Slime_bird_double_jump : MonoBehaviour
         anim.SetBool("doubleJump", false);
     }
 
-  
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //controllo che il giocatore sia su una cassa in aria , in modo da limitare il numero di salti effettuabili
+        if (collision.CompareTag("Object") && !collision.GetComponent<Object_logic>().onGround)
+        {
+            onBox = true;
+        }
+        //se la cassa è a terra allora il doppio salto è abilitato
+        if (collision.CompareTag("Object") && collision.GetComponent<Object_logic>().onGround)
+        {
+            onBox = false;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Object"))
+        {
+            onBox = false;
+        }
+    }
+
+
+
+
 }
