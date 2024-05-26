84e7c39f54a2ebb749d8ee0aacd38d721f253f84
Stefano Todaro <79905674+StefTodaro@users.noreply.github.com>
2024-05-26 13:15:02 +0200

    aggiunta roba che non si era aggiunta

---
+++ Assets/code/Mob_manager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Mob_manager : MonoBehaviour
+{
+    public List<GameObject> Mobs;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Mobs = FindObjectsOfType<Mob_respawn>().Select(o => o.gameObject).ToList();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(GameObject mob in Mobs)
+        {
+            if (!mob.active)
+            {
+                MobSpawn(mob);
+            }
+        }
+    }
+
+    public void MobSpawn(GameObject mob)
+    {
+
+        mob.GetComponent<Mob_respawn>().respawnTimer += Time.deltaTime;
+        Debug.Log(mob.GetComponent<Mob_respawn>().respawnTimer);
+
+        if (mob.GetComponent<Mob_respawn>().respawnTimer >= 12)
+        {
+            mob.GetComponent<ResettableObjects>().ResetState();
+        }
+    }
+}
